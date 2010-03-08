8e4f583877c1d0195665ce80bc2d16b9912210d8
Cary Lee <carylee@gmail.com>
2010-03-08 17:18:03 -0600

    Working on a geomap

--- gchart.php
+++ gchart.php
@@ -18,6 +18,10 @@ class GChart
       case "piechart":
         $this->type = 'PieChart';
         break;
+      case: "geomap_region":
+        $this->type = 'GeoMap';
+        $this->dataMode = 'regions';
+        break;
       default:
         $this->type = 'PieChart';
         break;
@@ -75,6 +79,7 @@ class GChart
   public $ylabel = '';
   public $type = 'PieChart';
   public $divName = 'chart_div';
+  private $dataMode;
 
   // Configuration Options
 
@@ -178,6 +183,12 @@ class GChart
     return $varChart;
   }
 
+  private function makeVarContainer() {
+    $varContainer = 'var container = document.getElementById(\''.$this->divName."');\n";
+    $varContainer .= "var geomap = new google.visualization.GeoMap(container);\n";
+    $varContainer .= "geomap.draw(data, options);\n";
+  };
+
   private function makeOptions() {
     $options = '';
     $options .= 'enableTooltip: ' . $this->enableTooltip . ', ';
@@ -203,6 +214,10 @@ class GChart
       $options .= 'titleFontSize: ' . $this->titleFontSize . ', ';
     }
 
+    if( $this->type == 'GeoMap') {
+      $options .= 'dataMode: ' . $this->dataMode . ', ';
+    }
+
     return $options;
   }
 
@@ -215,6 +230,8 @@ class GChart
     $drawChart = "function drawChart() {\n";
     $drawChart .= $this->makeVarData();
     $drawChart .= $this->makeVarChart();
+
+    //$drawChart .=
 
     $drawChart .= "\n}\n";
 
